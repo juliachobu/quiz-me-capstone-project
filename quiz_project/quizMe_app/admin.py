dcae7fb897f9fed04032c95bd5bd7d05368ac5cf
Julia Chobu <juliachobu@Julias-MacBook-Air.local>
2022-12-12 16:34:39 -0800

    Started project

---
+++ quiz_project/quizMe_app/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import quizQuestions
+
+admin.site.register(quizQuestions)
+
---
+++ quiz_project/quizMe_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QuizmeAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'quizMe_app'
